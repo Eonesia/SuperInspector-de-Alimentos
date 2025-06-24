f2ab2d2195cfd2497bea990f93fe40d6d8e3eca7
davidDLHG <ddelasherasgalera@gmail.com>
2025-06-24 10:38:57 +0200

    Shader Cartoon + Shader Seleccion

--- Assets/Scripts/PlayerInteractor.cs
+++ Assets/Scripts/PlayerInteractor.cs
@@ -15,6 +15,15 @@ public class PlayerInteractor : MonoBehaviour
     [Header("Debug")]
     public Color rayColor = Color.green;
 
+    [Header("Highlight Material")]
+    public Material highlightMaterial;  // Material para aplicar cuando se apunta al objeto
+
+    private GameObject lastHitObject = null;
+
+    // Guardamos los materiales originales para cada renderer
+    private Renderer[] lastRenderers = null;
+    private Material[][] lastOriginalMaterials = null;
+
     private void OnEnable()
     {
         interactInput.action.Enable();
@@ -27,21 +36,80 @@ public class PlayerInteractor : MonoBehaviour
         interactInput.action.Disable();
     }
 
-    private void OnInteract(InputAction.CallbackContext context)
+    private void Update()
     {
-        
         Ray ray = new Ray(transform.position, transform.forward);
-        
+
         if (Physics.Raycast(ray, out RaycastHit hit, rayDistance, interactableLayer))
         {
-            Debug.Log("Ray hit: " + hit.collider.name);
-            IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+            GameObject hitObject = hit.collider.gameObject;
+
+            if (hitObject != lastHitObject)
+            {
+                RestoreLastObjectMaterial();
+
+                ApplyHighlight(hitObject);
+
+                lastHitObject = hitObject;
+            }
+        }
+        else
+        {
+            RestoreLastObjectMaterial();
+            lastHitObject = null;
+        }
+    }
+
+    private void OnInteract(InputAction.CallbackContext context)
+    {
+        if (lastHitObject != null)
+        {
+            IInteractable interactable = lastHitObject.GetComponent<IInteractable>();
             if (interactable != null)
             {
                 interactable.Interact();
             }
-
         }
+    }
+
+    private void ApplyHighlight(GameObject obj)
+    {
+        // Conseguimos todos los Renderers en obj y sus hijos
+        lastRenderers = obj.GetComponentsInChildren<Renderer>();
+
+        // Guardamos los materiales originales de cada renderer
+        lastOriginalMaterials = new Material[lastRenderers.Length][];
+
+        for (int i = 0; i < lastRenderers.Length; i++)
+        {
+            lastOriginalMaterials[i] = lastRenderers[i].materials;
+
+            // Creamos array para asignar el material highlight en cada slot
+            Material[] highlightMats = new Material[lastOriginalMaterials[i].Length];
+            for (int j = 0; j < highlightMats.Length; j++)
+            {
+                highlightMats[j] = highlightMaterial;
+            }
+
+            lastRenderers[i].materials = highlightMats;
+        }
+    }
+
+    private void RestoreLastObjectMaterial()
+    {
+        if (lastRenderers != null && lastOriginalMaterials != null)
+        {
+            for (int i = 0; i < lastRenderers.Length; i++)
+            {
+                if (lastRenderers[i] != null)
+                {
+                    lastRenderers[i].materials = lastOriginalMaterials[i];
+                }
+            }
+        }
+
+        lastRenderers = null;
+        lastOriginalMaterials = null;
     }
 
     private void OnDrawGizmos()
@@ -49,6 +117,4 @@ public class PlayerInteractor : MonoBehaviour
         Gizmos.color = rayColor;
         Gizmos.DrawRay(transform.position, transform.forward * rayDistance);
     }
-
-   
 }
