3ab13f78a291f9ff6728c419b2edf828c5b65f91
FreSst1 <ricardo.arevaloferreira@gmail.com>
2025-06-24 10:45:00 +0200

    Coger objetos con el click izq

--- Assets/Scripts/Inventario/(PruebaInventario)PlayerInteract.cs
+++ Assets/Scripts/Inventario/(PruebaInventario)PlayerInteract.cs
@@ -5,20 +5,50 @@ public class PlayerInteract : MonoBehaviour
     public InventoryObject inventory;
     public Transform comidaCogida;
     public Transform Mano;
+    
+    
 
-    public void OnTriggerEnter(Collider other)
+
+
+void Update()
     {
-        var item = other.GetComponent<Item>();
-        if (item)
-        {
-            inventory.AddItem(item.item, 1);
-            comidaCogida = item.transform;
+        
 
-            comidaCogida.position = Mano.position;
-            comidaCogida.rotation = Mano.rotation;
-            comidaCogida.SetParent(Mano);
+        if (Input.GetMouseButtonDown(0)) // Clic izquierdo
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                var item = hit.collider.GetComponent<Item>();
+                if (item)
+                {
+                    inventory.AddItem(item.item, 1);
+                    Transform comidaCogida = item.transform;
+                    Rigidbody rb = comidaCogida.GetComponent<Rigidbody>();
+                    rb.isKinematic = true;
+                    comidaCogida.position = Mano.position;
+                    //comidaCogida.rotation = Mano.rotation;
+                    comidaCogida.SetParent(Mano);
+                }
+            }
         }
     }
+
+    //public void OnTriggerEnter(Collider other)
+    //{
+    //    var item = other.GetComponent<Item>();
+    //    if (item)
+    //    {
+    //        inventory.AddItem(item.item, 1);
+    //        comidaCogida = item.transform;
+
+    //        comidaCogida.position = Mano.position;
+    //        comidaCogida.rotation = Mano.rotation;
+    //        comidaCogida.SetParent(Mano);
+    //    }
+    //}
 
     private void OnApplicationQuit()
     {
