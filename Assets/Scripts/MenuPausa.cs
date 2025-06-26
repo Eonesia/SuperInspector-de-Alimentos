6aebc025a1abeb4a4c618f80ecd593f8b871b2bb
Paula Elices Rodríguez <pelicesr@gmail.com>
2025-06-26 10:32:04 +0200

    menu pausa
    el menu de pausa ya te lleva al menu inicial, vuelve al juego, etc, solo falta hacer que te lleve al menu de ajustes

--- Assets/Scripts/MenuPausa.cs
+++ Assets/Scripts/MenuPausa.cs
@@ -1,17 +1,32 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPausa : MonoBehaviour
 {
     public GameObject objetoMenuPausa;
+    public GameObject hud;
     public bool pausa = false;
 
     public void AlternarPausa()
     {
         pausa = !pausa;
         objetoMenuPausa.SetActive(pausa);
+        hud.SetActive(!pausa);
 
         Time.timeScale = pausa ? 0f : 1f;
         Cursor.lockState = pausa ? CursorLockMode.None : CursorLockMode.Locked;
         Cursor.visible = pausa;
+    }
+
+    public void IrAlMenuPrincipal()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("MenuInicio"); 
+    }
+
+    public void SalirDelJuego()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
     }
 }
