a557a91564e924690bb4225b2edf3e5ced977589
Paula Elices Rodríguez <pelicesr@gmail.com>
2025-07-28 10:40:51 +0200

    reloj en el tutorial

--- Assets/Scripts/TriggerCambioDeEscena.cs
+++ Assets/Scripts/TriggerCambioDeEscena.cs
@@ -1,18 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class TriggerCambioDeEscena : MonoBehaviour
 {
     public string nombreEscenaDestino = "MenuInicio";
 
-    private void OnTriggerEnter(Collider other)
+    // Acción pública para asignar desde otro script o inspector
+    public InputAction clickAction;
+
+    private Camera cam;
+
+    private void OnEnable()
     {
-        if (other.CompareTag("Player"))
+        clickAction.Enable();
+        clickAction.performed += OnClick;
+        cam = Camera.main;
+    }
+
+    private void OnDisable()
+    {
+        clickAction.performed -= OnClick;
+        clickAction.Disable();
+    }
+
+    private void OnClick(InputAction.CallbackContext context)
+    {
+        Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            SceneManager.LoadScene(nombreEscenaDestino);
-            Cursor.lockState = CursorLockMode.None;
-Cursor.visible = true;
-Time.timeScale = 1f;
+            if (hit.transform == transform) // Si el clic fue sobre este objeto
+            {
+                SceneManager.LoadScene(nombreEscenaDestino);
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                Time.timeScale = 1f;
+            }
         }
     }
 }
